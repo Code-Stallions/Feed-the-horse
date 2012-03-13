49f43e4747e730e48e11459297816fabced430e9
Andreas wiman <laja911@msn.com>
2012-03-13 21:35:16 +0100

    added tests for a running horse

--- NRH/JQuery/velesin-jasmine-jquery-7fcfa63/spec/ponySpec.js
+++ NRH/JQuery/velesin-jasmine-jquery-7fcfa63/spec/ponySpec.js
@@ -48,3 +48,146 @@ describe("FeedingForm", function(FoodCalculator) {zz
   
   
 });
+
+
+ describe("Food calc", function(){
+ 	it("It should give me the correct MJ", function(){
+ 	
+ 
+  	var MJ = 0.50 * Math.pow(600,0.75)	
+  	var CorrMJ = Math.ceil(MJ)
+ 		expect(CorrMJ).toEqual(61);
+ 		
+ 	});
+
+ 
+ 
+ 	
+ 	it("It should give me the correct Protein", function(){
+ 	
+ 
+  	var MJ = 0.50 * Math.pow(600,0.75)	
+  	var CorrMJ = Math.ceil(MJ)
+  	var Protein = CorrMJ * 6;
+ 		expect(Protein).toEqual(366);
+ 		
+ 	});
+
+ 
+ 
+ 
+ 	
+ 	it("It should give me the correct Kalcium", function(){
+ 	
+ 
+  	var MJ = 0.50 * Math.pow(600,0.75)	
+  	var CorrMJ = Math.ceil(MJ)
+  	var Kalcium = CorrMJ * 0.40;
+  	var KalciumCeil = Math.ceil(Kalcium)
+ 		expect(KalciumCeil).toEqual(25);
+ 		
+ 	});
+
+ 
+ 
+ 
+  	
+ 	it("It should give me the correct Fosfor", function(){
+ 	
+ 
+  	var MJ = 0.50 * Math.pow(600,0.75)	
+  	var CorrMJ = Math.ceil(MJ)
+  	var Fosfor = CorrMJ * 0.25;
+ 		expect(Fosfor).toEqual(15.25);
+ 		
+ 	});
+
+ 
+ 
+ 
+ 	it("It should give me the correct MG", function(){
+
+  	var MJ = 0.50 * Math.pow(600,0.75)	
+  	var CorrMJ = Math.ceil(MJ)
+  	var MG = CorrMJ * 0.15;
+  	
+	 		expect(MG).toEqual(9.15);
+ 		
+ 	});
+
+ 
+  
+ 			it("It should give me the correct SE", function(){
+ 	
+  			var Vikt = 600;	
+  			var SE = (Vikt / 100)*0.2;
+  			var SECeil = Math.ceil(SE)
+ 		expect(SECeil).toEqual(2);
+ 		
+ 	});
+
+ 
+  
+ 	it("It should give me the correct MG", function(){
+ 	
+ 
+  	var Vikt = 600;	
+  	
+  	var TS = (Vikt / 100)*1.75;
+ 		expect(TS).toEqual(10.5);
+ 		
+ 	});
+ });
+ 
+ 
+ 
+ 
+ 
+  describe("Springande", function(){
+ 		it("It should give me the correct skritt", function(){
+ 	
+ 
+  		
+  	var MJ = 0.50 * Math.pow(600,0.75)
+  	var Dagar = 5;	
+  	var Min = 30/10;
+  	
+  	var Skritt = 0.2*Dagar*Min;
+ 		expect(Skritt).toEqual(3);
+ 		
+ 	});
+ 	
+ 	
+ 	 	it("It should give me the correct MJ for sprint", function(){
+ 	
+ 
+  		
+  	var MJ = 0.50 * Math.pow(600,0.75)
+  	var Dagar = 5;	
+  	var Min = 60/10;
+  	
+  	var Sprint = 1.3*Dagar*Min;
+ 		expect(Sprint).toEqual(39);
+ 		
+ 	});
+ 	
+ 	
+ 		it("It should add the correct MJ for sprint and skritt", function(){
+ 	
+ 
+  		
+  	var MJ = 0.50 * Math.pow(600,0.75)
+  	var Dagar = 5;	
+  	var MinSprint = 60/10;
+  	var MinSkritt = 30/10;
+  	
+  	var Sprint = 1.3*Dagar*MinSprint;
+  	var Skritt = 0.2*Dagar*MinSkritt;
+  	
+  	var Running = Sprint + Skritt;
+ 		expect(Running).toEqual(42);
+ 		
+ 	});	
+ });
+ 
+
