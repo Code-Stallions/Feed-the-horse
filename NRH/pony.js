003d82fffa086d14144bee85ea66908446e4ba4c
MICHAEL <tmicahl@hotmail.com>
2012-03-14 13:50:46 +0100

    new JS file pony.js
    NEW JS file pony.js made for the index.html

--- NRH/pony.js
+++ NRH/pony.js
@@ -1,9 +1,97 @@
 
+function calc1() {
+			for (i=0;i<document.forms[0].horse.length;i++) {	
+				if (document.forms[0].horse[i].checked) {
+					var user_input = document.forms[0].horse[i].value;
+					var brk = "<br />";
+				}
+			}
+			var MJ = 0.5 * Math.pow(horseForm.weight.value,0.75);
+			if(user_input == "Lite"){
+				var feed = Math.ceil(MJ);
+			}
+			else if (user_input == "Normal"){
+				var feed = Math.ceil(MJ * 1.05);
+			}
+			else if (user_input == "Hard"){
+				var feed = Math.ceil(MJ *1.10);
+			} 
+			
+			var prot = Math.ceil(feed * 6);
+			var kal = Math.ceil(feed * 0.4);
+			var fos = Math.ceil(feed * 0.25);
+			var mag = Math.ceil(feed * 0.15);
+			var input = (horseForm.weight.value); 
+			var SE = Math.ceil(0.2*(input/100));
+			var TS = Math.ceil(1.7*(input/100));
 
-function FoodCalculater() {
-var a = 100;
-var b = 0.025;
-var c = a*b;
-document.write.display1(c);
-
-}
+			var food_input="";
+			for (j=0;j<document.form1.food.length;j++) {
+				if (document.form1.food[j].checked) {
+					food_input += document.form1.food[j].value + brk + "\n";
+				}
+			}
+			
+			var daysPerWeek = (horseForm.horseWorkout.value)/10;
+			var skritt = (horseForm.horseWorkoutSkritt.value);
+			var trav = (horseForm.horseWorkoutTrav.value);
+			
+			var skrittCalc =  Math.ceil(skritt * daysPerWeek * 0.2);
+			var travCalc =  Math.ceil(trav * daysPerWeek * 1.3);
+			
+			var workoutMJ =  Math.ceil(skrittCalc + travCalc + feed);
+			
+				document.write("<link rel='stylesheet' href='../NRH/CSS/style.css' type='text/css'/>");
+				document.write("<div class='display1'>");
+				document.write("<h1><u>Horse's Requirement</u></h1>");
+				document.write("Your horse's current weight is : " + input +"Kg");
+				document.write(brk);
+				document.write(brk);
+				document.write("You have checked  :  " + user_input);
+				document.write("<h4><u> Your Horse's Daily Requirement</u></h4>");
+				document.write("Total MJ : " + feed);
+				document.write(brk);
+				document.write(brk);
+				document.write("Total Protine : "+ prot +"g");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total Kalcium : " + kal +"g");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total Fosfor : " + fos +"g");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total Magnism : " + mag +"g");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total SE : " + SE + "g");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total TS : " + TS + "g");
+				document.write(brk);
+				document.write(brk);
+				document.write("<h4><u> Your Horse's Daily Requirement after Work-out</u></h4>");
+				document.write("Your horse works out : " + daysPerWeek * 10 +"</b> days a week");
+				document.write(brk);
+				document.write(brk);
+				document.write("Your horse Skritts for : " + skritt +"</b> minutes per workout");
+				document.write(brk);
+				document.write(brk);
+				document.write("Your horse Trav for : " + trav +"</b> minutes per workout");
+				document.write(brk);
+				document.write(brk);
+				document.write("Total för Skritt : " + skrittCalc);
+				document.write(brk);
+				document.write(brk);
+				document.write("Total för Trav : " + travCalc);
+				document.write(brk);
+				document.write(brk);
+				document.write("Total MJ för Work-out  : " + workoutMJ);
+				document.write(brk);
+				document.write(brk);
+				document.write("<h4><u>You are currently feeding your horse</u> </h4>" + food_input);
+				document.write("</div>");
+		  
+		  window.onload = calc1();
+		
+		}
