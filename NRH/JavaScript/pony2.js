b6ed8cbea20f37738751740a95b10b0a4e54230d
MICHAEL <tmicahl@hotmail.com>
2012-04-04 12:26:58 +0200

    new jquery updated version and also
    we are on jquery now with no more document.write on our JS

--- NRH/JavaScript/pony2.js
+++ NRH/JavaScript/pony2.js
@@ -1,5 +1,120 @@
+$(document).ready(function(){	
+					 $("#calcu").click(function(){
+   						
+   						var HorseType = $("input[name=horseType]:checked").val();
+   						
+   						var Horse = $("input[name=horse]:checked").val();
+   							
+   						
+   						var weight = parseInt($('#weight2').val(),0.75);
+						var weight2 = (5 * weight).toFixed(1);
+							
+							if (Horse === "Lite"){																							 
+								var feed = (weight2);
+								}						
+							else if (Horse === "Normal"){																							 
+								var feed = (weight2 * 1.05);
+								}																															 
+							else if (Horse === "Hard"){																								 
+							 	var feed = (weight2 * 1.10);
+								}
+						
+						var prot = (feed * 6).toFixed(1);				// It works out the SMRC math formulas//
+						
+						var kal = (feed * 0.4).toFixed(1);				// It works out the calciam math formulas//
+						
+						var fos = (feed * 0.25).toFixed(1);				// It works out the fosfers math formulas//
+						
+						var mag = (feed * 0.15).toFixed(1);				// It works out the magnizam math formulas//
+						
+						var SE = (0.2* (weight2/100)).toFixed(1) ;		// It works out the SE math formulas//
+					
+						var TS = (1.5* (weight2/100)).toFixed(1);		// It works out the TS math formulas//
+				
+						
+						var MJinput = parseInt($('#MjInput').val());
+						var SmbrpInput = parseInt($('#SmbrpInput').val());
+						
+						
+						var Horsefiger = $("input[name=horsefiger]:checked").val();
+							if (Horsefiger === "Small"){																							 
+								 var sizeType = ("<p>This horse is too small need to eat</p>");
+								}						
+							
+							else if (Horsefiger === "Normall"){																							 
+								  var sizeType =("<p>This horse is normal need to eat</p>");
+								}																															 
+							
+							else if (Horsefiger === "Tjok"){																								 
+							 	 var sizeType = ("<p>This horse is too fat need to eat</p>");
+								}
+						
+						var HayType = $("input[name=tableCall]:checked").val();
+						
+						var MJenergy = parseInt($('#MJ1').val());
+						var SMBRPenergy = parseInt($('#SMBRP2').val());
+						var Caenergy = parseInt($('#Ca3').val());
+						var Penergy = parseInt($('#Phos4').val());
+						var Mgenergy = parseInt($('#MG5').val());
+						var SEenergy = parseInt($('#SE6').val());
+						var TSenergy = parseInt($('#TS7').val());
+						
+						var days = parseInt($('#horseWorkout').val());						// It works out the days Per Week math formulas//
+						var daysPerWeek = (days/10);														
+						
+						var skrit1 = parseInt($('#horseWorkoutSkritt').val());				// It gets the value of the horse Workout Skritt//
+						var skrittCalc = ((skrit1 * daysPerWeek) * 0.2);					
+						var skrittCalc2 = (skrittCalc).toFixed(1);	
+						
+						var TravAmout = parseInt($('#horseWorkoutTrav').val());				// It gets the value of the horse Workout Trav//
+						var travCalc = ((TravAmout * daysPerWeek) * 1.3);							
+						var travCalc2 = (travCalc).toFixed(1);
+						
+						var workoutMJ = (skrittCalc + travCalc + feed);			// It works out how much MJ needs to be added after the horse works out//
+						
+						var foodType = $("input[type='checkbox'][checked=true]")
+			
+			$("#time").html(
+						HorseType +'<br/>'
+						+ Horse +'<br/>'
+						+ feed +" Kg" + '<br/>'
+						+ prot+" g"+'<br/>'
+						+ kal+" g"+'<br/>'
+						+ fos+" g"+'<br/>'
+						+ mag+" g"+'<br/>'
+						+ SE +" g"+'<br/>'
+						+ TS +" g"+'<br/>'
+						
+						+ MJinput +" g"+'<br/>'
+						+  SmbrpInput +" g"+'<br/>'
+						+ Horsefiger +'<br/>'
+						+ sizeType +'<br/>'
+						+ HayType +'<br/>'
+						
+						+ 'MJ :' +  MJenergy +'Kg'
+						+ ' &nbsp;'+ 'Protine :' +  SMBRPenergy +'g'
+						+ ' &nbsp;'+ 'Calcium :' +  Caenergy +'g'
+						+ ' &nbsp;'+ 'Fosfers :' +  Penergy +'g'
+						+ ' &nbsp;'+ 'Magnisum :' +   Mgenergy +'g'
+						+ ' &nbsp;'+ 'SE :' +   SEenergy +'g'
+						+ ' &nbsp;'+ 'TS :' +   TSenergy +'g'+'<br/>'
+						
+						+ daysPerWeek +" days per week"+'<br/>'
+						+ skrittCalc + " Skrit hours per week" + '<br/>'
+						+ travCalc +" Trav hours in week"+'<br/>'
+						+ workoutMJ +" Kg : MJ after working out "+'<br/>'
+						+ 'food :' +  foodType +'<br/>' +':'
+						);
+  					
+  					});
+				});
+ 
+ 
 
-function calc1() {
+
+
+
+/*function calc1() {
 			
 			for (i=0;i<document.forms[0].horseType.length;i++) {			// It loops to find horse type//
 				if (document.forms[0].horseType[i].checked) {
@@ -70,12 +185,7 @@ function calc1() {
 			
 			var workoutMJ = (skrittCalc + travCalc + feed).toFixed(1);			// It works out how much MJ needs to be added after the horse works out//
 				
-		function ponyMJ() {
 		
-		
-		
-		}		
-				
 				document.write("<link rel='stylesheet' href='../NRH/CSS/style1.css' type='text/css'/>");
 				document.write("<div class='display'>");
 				document.write("<h1><u>Horse's Requirement</u></h1>");
@@ -133,4 +243,6 @@ function calc1() {
 		  window.onload = calc1();
 		
 		}
-		
+
+
+});*/
